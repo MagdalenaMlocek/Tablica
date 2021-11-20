5aa6adedbf012f7b324f1d126d21fdd31caff616
Magda <magdalena.mlocek20@gmail.com>
2021-11-20 10:01:23 +0100

    tablica petla

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] tablica = {1,2,3,4,5,6};
+        for(int i = 0; i < tablica.length; i++){
+            if( tablica[i] < 3){
+                System.out.println("True");
+            } else if(tablica[i] < 5){
+                System.out.println("False");}
+            };
+        }
+    }
+
