7329166d8f1c12482bb3cf266fc72cbfa3273b8d
Magda <magdalena.mlocek20@gmail.com>
2021-11-20 14:56:54 +0100

    tablica petla

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,61 @@
 public class Main {
     public static void main(String[] args) {
-        int [] tablica = {1,2,3,4,5,6};
-        for(int i = 0; i < tablica.length; i++){
-            if( tablica[i] < 3){
-                System.out.println("True");
-            } else if(tablica[i] < 5){
-                System.out.println("False");}
-            };
+//        int [] tablica = {1,2,3,4,5,6};
+//        for (int liczba:tablica) {
+//            if (liczba > 3 && liczba < 5)
+//            System.out.println(liczba);
+//        }
+//        for(int i = 0; i < tablica.length; i++){
+//            if( tablica[i] < 3){
+//                System.out.println("True");
+//            } else if(tablica[i] < 5){
+//                System.out.println("False");}
+//            };
+
+//        String[] cars ={"audi", "nissan", "renault", "volvo"};
+//        for(String car:cars){
+//            System.out.println(car);
+//        };
+//        int day = 3;
+//        switch (day) {
+//            case 1:
+//                System.out.println("Mondey");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            default:
+//                System.out.println("Weekend");
+//
+//        }
+//----------------------------------------Petla while----------------------
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        -------------------------------Petla do while-----------------
+//        int j = 0;
+//        do {
+//            System.out.println(j);
+//            j++;
+//        }
+//        while (j < 5);
+
+        for (int i = 0; i < 10; i++){
+            if (i == 5) {
+                int x;
+                x = i;
+                System.out.println(x);
+                continue;
+            }
+            System.out.println(i);
         }
+
+
+    }
+
+
     }
 
